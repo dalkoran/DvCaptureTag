397c06045cbdcd2a2d04aab45ab8b99536fa4e2a
Nigel Spencer <nigel@spencen.com>
2020-10-04 17:12:32 -0400

    Add LogLevel and TagOverrides. Add FrameRate and secondary Timcode parsing. Refine total status and alerting around writable and total updates. Add Docker support.

--- DVCaptureTag.Core.Console/Program.cs
+++ DVCaptureTag.Core.Console/Program.cs
@@ -2,14 +2,24 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Text.RegularExpressions;
     using CommandLine;
+    using CommandLine.Text;
     using MediaInfo;
 
     ////using MediaInfo;
 
     class Program
     {
+        private static Regex recordedDateRegex = new Regex(@"^\s*Recorded date\s*:\s(?<RecordedDate>[0-9\- :.]*)", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static Regex tapeNameRegex = new Regex(@"^\s*TAPE\s*:\s(?<TapeName>\w*)", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static Regex timecodeInRegex = new Regex(@"^\s*TCOD\s*:\s(?<TimeCode>[0-9]*)", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static Regex timecodeOutRegex = new Regex(@"^\s*TCDO\s*:\s(?<TimeCode>[0-9]*)", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static Regex captureStatsRegex = new Regex(@"^\s*STAT\s*:\s(?<TotalFrames>[0-9]*)\s(?<FramesDropped>[0-9]*)\s(?<Rate>[0-9.]*)\s(?<Other>[0-9]*)\s*", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static Regex timecodeFirstRegex = new Regex(@"^Time code of first frame\s*:\s(?<TimeCode>(?<Hour>[0-9]{2}):(?<Minute>[0-9]{2}):(?<Second>[0-9]{2}):(?<Frame>[0-9]*))", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static Regex frameRateRegex = new Regex(@"^Frame rate\s*:\s(?<FramesPerSecond>[0-9.]*)\sFPS\s*", RegexOptions.Multiline | RegexOptions.Compiled);
+
         public class Options
         {
             [Option('f', "folderPath", Required = true)]
@@ -32,16 +42,26 @@
 
             [Option('w', "waitOnEnter", Default = false, HelpText = "The command pauses prior to completion awaiting input from the user.", Hidden = true)]
             public bool WaitOnEnter { get; set; }
+
+            [Option('l', "logLevel", Default = LogLevel.Error, HelpText = "Sets the log level used by the MediaInfo library.")]
+            public LogLevel LogLevel { get; set; }
+
+            [Option('o', "allowTagOverrides", Default = false, HelpText = "Set to true to allow existing tag values to be overritten.")]
+            public bool AllowTagOverrides { get; set; }
         }
 
         public struct DVMetadata
         {
+            public const double DefaultFrameRatePerSecond = 25.0;
+            public const ulong TimecodeFactor = 10000000;
+
             public DateTime? RecordedDate;
             public string TapeName;
             public ulong? TimecodeIn;
             public ulong? TimecodeOut;
             public uint? TotalFrames;
             public uint? DroppedFrames;
+            public double? FrameRatePerSecond;
 
             public string TimecodeString
             {
@@ -49,27 +69,34 @@
                 {
                     if (this.TimecodeIn.HasValue && this.TimecodeOut.HasValue)
                     {
-                        return $"{TimecodeToString(this.TimecodeIn)} - {TimecodeToString(this.TimecodeOut)}";
+                        return $"{TimecodeToString(this.TimecodeIn, this.FrameRatePerSecond)} - {TimecodeToString(this.TimecodeOut, this.FrameRatePerSecond)}";
+                    }
+                    else if (this.TimecodeIn.HasValue)
+                    {
+                        return TimecodeToString(this.TimecodeIn, this.FrameRatePerSecond);
                     }
 
                     return null;
                 }
             }
 
-            public static string TimecodeToString(ulong? timecode)
+            public static string TimecodeToString(ulong? timecode, double? frameRatePerSecond)
             {
-                var frame = (timecode % 10000000) / 10000000.0;
-                var time = new TimeSpan(0, 0, (int)(timecode / 10000000));
+                var frame = (timecode % TimecodeFactor) / (double)TimecodeFactor;
+                var time = new TimeSpan(0, 0, (int)(timecode / TimecodeFactor));
 
-                return $"{time.Hours:00}:{time.Minutes:00}:{time.Seconds:00}:{(int)(frame * 25) + 1:00}";
+                return $"{time.Hours:00}:{time.Minutes:00}:{time.Seconds:00}:{(int)(frame * (frameRatePerSecond ?? DefaultFrameRatePerSecond)) + 1:00}";
             }
         }
 
         public class Stats
         {
-            public uint NumberOfFilesProcessed { get; set; }
-            public uint NumberOfFileTagsUpdated { get; set; }
-            public uint NumberOfFileDatesUpdated { get; set; }
+            public uint NumberOfFilesChecked { get; set; }
+            public uint NumberOfFileTagsChanged { get; set; }
+            public uint NumberOfFilesWithTagChanges { get; set; }
+            public uint NumberOfFileCreationDatesChanged { get; set; }
+            public uint NumberOfFilesSavedWithTagsUpdated { get; set; }
+            public uint NumberOfFilesSaveWithCreationDateUpdated { get; set; }
         }
 
         static void Main(string[] args)
@@ -84,9 +111,17 @@
                         ? SearchOption.AllDirectories
                         : SearchOption.TopDirectoryOnly;
 
+                    var enumerationOptions = new EnumerationOptions()
+                    {
+                        IgnoreInaccessible = true,
+                        MatchType = MatchType.Simple,
+                        RecurseSubdirectories = searchOptions == SearchOption.AllDirectories,
+                        ReturnSpecialDirectories = false,
+                    };
+
                     Console.WriteLine($"Scanning folder {folderInfo.FullName} {(options.RecurseChildFolders ? "recursively " : "")}for files matching {options.Pattern}:");
 
-                    foreach (var file in folderInfo.GetFiles(options.Pattern, searchOptions))
+                    foreach (var file in folderInfo.GetFiles(options.Pattern, enumerationOptions).OrderBy(fi => fi.FullName))
                     {
                         var path = file.FullName;
                         var metadata = GetDVMetadata(path, options);
@@ -94,13 +129,22 @@
                         UpdateViaTagLib(path, metadata, options, stats);
                         UpdateFileCreationTime(path, metadata, options, stats);
 
-                        stats.NumberOfFilesProcessed++;
+                        stats.NumberOfFilesChecked++;
                     }
 
                     if (!options.Quiet) Console.WriteLine();
-                    Console.WriteLine($"Number of matching files found: {stats.NumberOfFilesProcessed}");
-                    Console.WriteLine($"Number of file tags updated:    {stats.NumberOfFileTagsUpdated}");
-                    Console.WriteLine($"Number of creation dates set:   {stats.NumberOfFileDatesUpdated}");
+                    Console.WriteLine($"Number of matching files found:  {stats.NumberOfFilesChecked}");
+                    Console.WriteLine($"Number of file tags changed:     {stats.NumberOfFilesWithTagChanges} files {stats.NumberOfFileTagsChanged} tags");
+                    Console.WriteLine($"Number of creation dates set:    {stats.NumberOfFileCreationDatesChanged}");
+                    if (!options.PerformUpdate)
+                    {
+                        Console.WriteLine("** No file updates perfomed: use -u/--performUpdate switch to actually apply file updates.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Number of files with tags saved: {stats.NumberOfFilesSavedWithTagsUpdated}");
+                        Console.WriteLine($"Number of files with date saved: {stats.NumberOfFilesSaveWithCreationDateUpdated}");
+                    }
 
                     if (options.WaitOnEnter)
                     {
@@ -111,8 +155,13 @@
 
         private static DVMetadata GetDVMetadata(string path, Options options)
         {
+            if (!options.Quiet)
+            {
+                Console.WriteLine(path);
+            }
+
             // This first line is used just to ensure we have the MediaInfo library loaded.
-            var logger = new Logger();
+            var logger = new Logger(options.LogLevel);
             var mediaInfo = new MediaInfoWrapper(path, logger);
 
             var filePtr = new MediaInfo();
@@ -121,33 +170,51 @@
             try
             {
                 var result = filePtr.Inform();
+                if (!options.Quiet && options.LogLevel <= LogLevel.Verbose)
+                {
+                    Console.WriteLine($"  {result}");
+                }
 
-                var recordedDateMatch = Regex.Match(result, @"^\s*Recorded date\s*:\s(?<RecordedDate>[0-9\- :.]*)", RegexOptions.Multiline);
-                var tapeNameMatch = Regex.Match(result, @"^\s*TAPE\s*:\s(?<TapeName>\w*)", RegexOptions.Multiline);
-                var timecodeInMatch = Regex.Match(result, @"^\s*TCOD\s*:\s(?<TimeCode>[0-9]*)", RegexOptions.Multiline);
-                var timecodeOutMatch = Regex.Match(result, @"^\s*TCDO\s*:\s(?<TimeCode>[0-9]*)", RegexOptions.Multiline);
-                var captureStatsMatch = Regex.Match(result, @"^\s*STAT\s*:\s(?<TotalFrames>[0-9]*)\s(?<FramesDropped>[0-9]*)\s(?<Rate>[0-9.]*)\s(?<Other>[0-9]*)\s*", RegexOptions.Multiline);
+                var recordedDateMatch = recordedDateRegex.Match(result);
+                var tapeNameMatch = tapeNameRegex.Match(result);
+                var timecodeInMatch = timecodeInRegex.Match(result);
+                var timecodeOutMatch = timecodeOutRegex.Match(result);
+                var captureStatsMatch = captureStatsRegex.Match(result);
+                var timecodeFirstMatch = timecodeFirstRegex.Match(result);
+                var frameRateMatch = frameRateRegex.Match(result);
 
                 DVMetadata output = new DVMetadata();
                 if (DateTime.TryParse(recordedDateMatch.Groups["RecordedDate"]?.Value, out DateTime recordedDate)) output.RecordedDate = recordedDate;
                 output.TapeName = tapeNameMatch.Groups["TapeName"]?.Value;
-                if (ulong.TryParse(timecodeInMatch.Groups["TimeCode"]?.Value, out ulong timecodeIn)) output.TimecodeIn = timecodeIn;
+                if (double.TryParse(frameRateMatch.Groups["FramesPerSecond"].Value, out double frameRatePerSecond)) output.FrameRatePerSecond = frameRatePerSecond;
+                if (ulong.TryParse(timecodeInMatch.Groups["TimeCode"]?.Value, out ulong timecodeIn))
+                {
+                    output.TimecodeIn = timecodeIn;
+                }
+                else
+                {
+                    if (timecodeFirstMatch.Success)
+                    {
+                        output.TimecodeIn = 
+                            (ulong.Parse(timecodeFirstMatch.Groups["Hour"].Value) * 60 * 60 * DVMetadata.TimecodeFactor) +
+                            (ulong.Parse(timecodeFirstMatch.Groups["Minute"].Value) * 60 * DVMetadata.TimecodeFactor) +
+                            (ulong.Parse(timecodeFirstMatch.Groups["Second"].Value) * DVMetadata.TimecodeFactor) + 
+                            (ulong)(double.Parse(timecodeFirstMatch.Groups["Frame"].Value) / (output.FrameRatePerSecond ?? DVMetadata.DefaultFrameRatePerSecond) * DVMetadata.TimecodeFactor);
+                    }
+                }
                 if (ulong.TryParse(timecodeOutMatch.Groups["TimeCode"]?.Value, out ulong timecodeOut)) output.TimecodeOut = timecodeOut;
                 if (uint.TryParse(captureStatsMatch.Groups["TotalFrames"]?.Value, out uint totalFrames)) output.TotalFrames = totalFrames;
                 if (uint.TryParse(captureStatsMatch.Groups["FramesDropped"]?.Value, out uint droppedFrames)) output.DroppedFrames = droppedFrames;
 
-                if (!options.Quiet)
+                if (!options.Quiet && options.Verbose)
                 {
-                    Console.WriteLine(path);
-                    if (options.Verbose)
-                    {
-                        Console.WriteLine($"    Recorded Date:   {output.RecordedDate}");
-                        Console.WriteLine($"    Tape Name:       {output.TapeName}");
-                        Console.WriteLine($"    Timecode In:     {output.TimecodeIn}");
-                        Console.WriteLine($"    Timecode Out:    {output.TimecodeOut}");
-                        Console.WriteLine($"    Total Frames:    {output.TotalFrames}");
-                        Console.WriteLine($"    Dropped Frames:  {output.DroppedFrames}");
-                    }
+                    Console.WriteLine($"    Recorded Date:   {output.RecordedDate}");
+                    Console.WriteLine($"    Tape Name:       {output.TapeName}");
+                    Console.WriteLine($"    Timecode In:     {output.TimecodeIn}");
+                    Console.WriteLine($"    Timecode Out:    {output.TimecodeOut}");
+                    Console.WriteLine($"    Total Frames:    {output.TotalFrames}");
+                    Console.WriteLine($"    Frame rate:      {output.FrameRatePerSecond} FPS");
+                    Console.WriteLine($"    Dropped Frames:  {output.DroppedFrames}");
                 }
 
                 return output;
@@ -172,8 +239,9 @@
                     if (options.PerformUpdate)
                     {
                         osFile.CreationTime = metadata.RecordedDate.Value;
+                        stats.NumberOfFilesSaveWithCreationDateUpdated++;
                     }
-                    stats.NumberOfFileDatesUpdated++;
+                    stats.NumberOfFileCreationDatesChanged++;
                     osFile.Refresh();
                 }
             }
@@ -183,33 +251,72 @@
         {
             using (var tagFile = TagLib.File.Create(path))
             {
-                if (metadata.TapeName != null && tagFile.Tag.Title != metadata.TapeName)
+                var tagsUpdated = false;
+
+                if (!tagFile.Writeable && !options.Quiet)
                 {
-                    tagFile.Tag.Title = metadata.TapeName;
-                    stats.NumberOfFileTagsUpdated++;
-                    if (!options.Quiet) Console.WriteLine($"  Title tag updated to match Tape Name: {metadata.TapeName}");
+                    Console.WriteLine($"  Tags are not writable for this file.");
                 }
 
-                if (metadata.TimecodeString != null && tagFile.Tag.Comment != metadata.TimecodeString)
+                if (metadata.TapeName != null)
                 {
+                    if ((tagFile.Tag.Album ?? string.Empty) != (metadata.TapeName ?? string.Empty))
+                    {
+                        if (!options.Quiet) Console.WriteLine($"  Album tag updated from '{tagFile.Tag.Album}' to match tape name: '{metadata.TapeName}'");
+                        tagFile.Tag.Album = metadata.TapeName;
+                        stats.NumberOfFileTagsChanged++;
+                        tagsUpdated = true;
+                    }
+
+                    if ((tagFile.Tag.Title ?? string.Empty) != (metadata.TapeName ?? string.Empty) &&
+                        (options.AllowTagOverrides || string.IsNullOrWhiteSpace(tagFile.Tag.Title)))
+                    {
+                        if (!options.Quiet) Console.WriteLine($"  Title tag updated from '{tagFile.Tag.Title}' to match tape name: '{metadata.TapeName}'");
+                        tagFile.Tag.Title = metadata.TapeName;
+                        stats.NumberOfFileTagsChanged++;
+                        tagsUpdated = true;
+                    }
+                }
+
+                if (metadata.TimecodeString != null && 
+                    tagFile.Tag.Comment != metadata.TimecodeString && 
+                    (options.AllowTagOverrides || string.IsNullOrWhiteSpace(tagFile.Tag.Comment)))
+                {
+                    if (!options.Quiet) Console.WriteLine($"  Comment tag updated from '{tagFile.Tag.Comment}' to include timecode information: '{metadata.TimecodeString}'");
                     tagFile.Tag.Comment = metadata.TimecodeString;
-                    stats.NumberOfFileTagsUpdated++;
-                    if (!options.Quiet) Console.WriteLine("  Comment tag updated to include Timecode information.");
+                    stats.NumberOfFileTagsChanged++;
+                    tagsUpdated = true;
                 }
 
-                if (options.PerformUpdate)
+                if (tagsUpdated)
                 {
-                    tagFile.Save();
-                    Console.WriteLine("  Tags saved.");
+                    stats.NumberOfFilesWithTagChanges++;
+
+                    if (options.PerformUpdate && tagFile.Writeable)
+                    {
+                        tagFile.Save();
+                        Console.WriteLine("  Tags saved.");
+                        stats.NumberOfFilesSavedWithTagsUpdated++;
+                    }
                 }
             }
         }
 
         public class Logger : ILogger
         {
+            public Logger(LogLevel logLevel)
+            {
+                this.LogLevel = logLevel;
+            }
+
+            public LogLevel LogLevel { get; set; }
+
             public void Log(LogLevel loglevel, string message, params object[] parameters)
             {
-                ////Console.WriteLine($"{loglevel}: {message}", parameters);
+                if (loglevel >= this.LogLevel)
+                {
+                    Console.WriteLine($"  {loglevel}: {message}", parameters);
+                }
             }
         }
     }
